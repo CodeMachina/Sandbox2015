17f6d5f276fb3bd9b4ed1c35f81b211380a5074f
CodeMachina <smflemin@gmail.com>
2015-07-27 19:48:21 -0400

    site setup

---
+++ Sandbox2015Site/SandboxSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SandboxSite.Startup))]
+namespace SandboxSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
