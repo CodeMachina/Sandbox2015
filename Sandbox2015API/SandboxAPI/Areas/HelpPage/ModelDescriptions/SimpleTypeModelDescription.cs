6267616d6165cc583ac235d04da277ca5fef6391
CodeMachina <smflemin@gmail.com>
2015-07-27 19:50:25 -0400

    initial api setup

---
+++ Sandbox2015API/SandboxAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace SandboxAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Sandbox2015API/SandboxAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SandboxAPI.WebApiApplication" Language="C#" %>
