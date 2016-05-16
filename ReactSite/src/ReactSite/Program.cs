48ec9df282deb27c4fe5213181f8cf14f2190544
Scott Fleming <assaultbr@hotmail.com>
2016-05-16 18:36:11 -0400

    Updated for .net core rc2

---
+++ ReactSite/src/ReactSite/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace ReactSite {
+    public class Program
+    {
+        public static void Main(string[] args) {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
--- ReactSite/src/ReactSite/Startup.cs
+++ ReactSite/src/ReactSite/Startup.cs
@@ -1,14 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNet.Builder;
-using Microsoft.AspNet.Hosting;
-using Microsoft.AspNet.Http;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace ReactSite
-{
+namespace ReactSite {
     public class Startup
     {
         // This method gets called by the runtime. Use this method to add services to the container.
@@ -21,8 +14,6 @@ namespace ReactSite
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
-            app.UseIISPlatformHandler();
-
             app.UseStaticFiles();
 
             app.UseMvc(routes => {
@@ -32,8 +23,5 @@ namespace ReactSite
                 );
             });
         }
-
-        // Entry point for the application.
-        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
     }
 }
